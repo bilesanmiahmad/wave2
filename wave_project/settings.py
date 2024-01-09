c92c0d53ba77d9c59512877f20a62c42b7d34464
Bilesanmi Ahmad <fbilesanmi@gmail.com>
2024-01-09 12:59:55 +0000

    update settings.py to add more CORS settings

--- wave_project/settings.py
+++ wave_project/settings.py
@@ -143,3 +143,25 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS: True
 
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
+
+CORS_ALLOW_HEADERS = (
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+)
